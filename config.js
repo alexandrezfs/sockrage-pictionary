fcf9ca456ab5f42ae895afec45b39d36686f1773
root <root@akira.dev.lenz.jp>
2014-08-26 11:54:26 +0900

    Adding config.js to gitignore

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * @type {{super_admin_password: string, server_port: number}}
  */
 exports.configObject = {
-    sockrage_addr : "http://localhost:3000",
+    sockrage_addr : "http://192.168.1.125:3000",
     db_name : 'pictionary',
     server_port : 3003
 };
